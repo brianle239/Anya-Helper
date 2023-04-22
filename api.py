7eb28b42c9a1b732991f87185fe72aa1cb518d36
celia chamu <chamu.celia@gmail.com>
2023-04-22 05:35:45 -0700

    fixed some api keys'

--- api.py
+++ api.py
@@ -7,13 +7,13 @@ async def root():
 
 @app.get("/sleep")
 async def root():
-    return {"msg1":"Good Night", "msg2": "Sweet Dreams", "msg3": "Good Luck Tomorrow"}
+    return {0:"Good Night", 1: "Sweet Dreams", 2: "Good Luck Tomorrow"}
 
 @app.get("/dissapointed")
 async def root():
-    return {"msg1":"", "msg": "Sweet Dreams"}
+    return {0:"...", 1: "I'm not mad, just Dissapointed", 2: "Get back to work"}
 
 @app.get("/watching")
 async def root():
-    return {"msg1":"I See you!", "msg2": "Get Back To Work Now", "msg3": "I am always watching"}
+    return {0:"I See you!", 1: "Get Back To Work Now", 2: "I am always watching"}
     
